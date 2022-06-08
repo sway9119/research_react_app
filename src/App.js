8ea4cf2544eafa6aad959f50130eb70e5159f53e
sway <40759948+sway9119@users.noreply.github.com>
2022-06-08 17:59:45 +0900

    都道府県一覧を取得する機能を追加

--- src/App.js
+++ src/App.js
@@ -2,8 +2,38 @@ import React from 'react';
 
 class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      prefectures: {} // 都道府県一覧データ郡
+    };
+  }
+
+  componentDidMount() {
+    // 47都道府県一覧を取得する
+    const apiKey = process.env.REACT_APP_RESAS_API_KEY;
+    fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', 
+      { headers: { 'X-API-KEY': apiKey }}
+    )
+    .then(response => response.json()) // fetchApiで取得したリクエスト結果をjsonにする
+    .then(res => {
+      this.setState({ prefectures: res.result });
+    });
+  }
+
+  createItem(props) {
+    return (
+      <div key={props.prefCode} >{props.prefCode}: {props.prefName}</div>
+    );
+  }
+
   render() {
-    return <h1>Hello, World!</h1>;
+    const prefs = this.state.prefectures;
+    return (  
+      <div>
+        {Object.keys(prefs).map(i => this.createItem(prefs[i]))}
+      </div>
+    );
   }
 };
 export default App;
