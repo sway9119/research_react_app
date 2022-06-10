9fbc43253c8af5b9c0402505a8b563fa4d23e725
sway <40759948+sway9119@users.noreply.github.com>
2022-06-10 18:36:06 +0900

    都道府県選択チェックボックスをコンポーネント化

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React from 'react';
+import CheckBoxs from "./components/CheckBoxs";
 
-class App extends React.Component {
+function App() {
+  return (
+    <div className="App">
+      <h1>Research_react_app</h1>
+      <CheckBoxs />
+    </div>
+  );
+}
 
-  constructor() {
-    super();
-    this.state = {
-      prefectures: {} // 都道府県一覧データ郡
-    };
-  }
-
-  componentDidMount() {
-    // 47都道府県一覧を取得する
-    const apiKey = process.env.REACT_APP_RESAS_API_KEY;
-    fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', 
-      { headers: { 'X-API-KEY': apiKey }}
-    )
-    .then(response => response.json()) // fetchApiで取得したリクエスト結果をjsonにする
-    .then(res => {
-      this.setState({ prefectures: res.result });
-    });
-  }
-
-  createItem(props) {
-    return (
-      <div key={props.prefCode} >{props.prefCode}: {props.prefName}</div>
-    );
-  }
-
-  render() {
-    const prefs = this.state.prefectures;
-    return (  
-      <div>
-        {Object.keys(prefs).map(i => this.createItem(prefs[i]))}
-      </div>
-    );
-  }
-};
 export default App;
